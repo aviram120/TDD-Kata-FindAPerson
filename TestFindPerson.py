2743900f157dea85a7284a7d79b6e30ac8f15d0b
aviram120 <aviram1202@gmail.com>
2015-05-20 11:12:53 +0300

    RED: find person posts test
    write the TDD

--- TestFindPerson.py
+++ TestFindPerson.py
@@ -1,4 +1,8 @@
 import unittest
 from crowdmap import Crowdmap
 
+class FindPersonTests(unittest.TestCase):
+    def setUp(self):
+        self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley",
+                                  "Or in Tel-Aviv","Or in Las Vegas"])
 
