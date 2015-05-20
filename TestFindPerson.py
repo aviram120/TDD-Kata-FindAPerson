6282b579de5169af759eb3b5b3abf637f36d7511
aviram120 <aviram1202@gmail.com>
2015-05-20 11:22:04 +0300

    GREEN:pass

--- TestFindPerson.py
+++ TestFindPerson.py
@@ -6,6 +6,6 @@ class FindPersonTests(unittest.TestCase):
         self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley",
                                   "Or in Tel-Aviv","Or in Las Vegas"])
 
-      def test_getAllPostsForName(self):
-       posts = self.crowdmap.get_all_posts_for("Or")
-       self.assertIn("Or in Cuba",posts)
+    def test_getAllPostsForName(self):
+        posts = self.crowdmap.get_all_posts_for("Or")
+        self.assertIn("Or in Las Vegas",posts)
