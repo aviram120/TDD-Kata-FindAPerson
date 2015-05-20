d3d836121efd18ccaaa03669f4b58471e6d03ea3
aviram120 <aviram1202@gmail.com>
2015-05-20 11:08:50 +0300

    initial commit
    starting new project

--- TestFindPerson.py
+++ TestFindPerson.py
@@ -1,18 +1,4 @@
 import unittest
 from crowdmap import Crowdmap
 
-class FindPersonTests(unittest.TestCase):
-    def setUp(self):
-        self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
 
-    def test_getAllPostsForName(self):
-       posts = self.crowdmap.get_all_posts_for("Or")
-       self.assertIn("Or",posts)
-
-    def test_get_all_posts_for_missing_name(self):
-        posts = self.crowdmap.get_all_posts_for("Or2")
-        self.assertFalse(posts)
-
-    def test_homework_examlpe(self):
-        posts = self.crowdmap.get_all_posts_for("Or2")
-        self.assertIn("Or A", posts)
