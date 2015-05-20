afbb39a60469980965db212cc714a1e37162f5ff
aviram120 <aviram1202@gmail.com>
2015-05-20 11:19:58 +0300

    function to test

--- crowdmap.py
+++ crowdmap.py
@@ -1,1 +1,12 @@
 __author__ = 'aviram'
+
+class Crowdmap():
+    def __init__(self, l_post):
+        self.posts_list = l_post
+
+    def get_all_posts_for(self, name):
+        list_of_posts = list()
+        for l in self.posts_list:
+            if name in l:
+                list_of_posts.append(l)
+        return list_of_posts
