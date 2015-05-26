99c531a88bab776b4cedc8c9d1061a489a5df0e5
aviram120 <aviram1202@gmail.com>
2015-05-26 19:49:53 +0300

    RED:failing test area square

--- TestMatFun.py
+++ TestMatFun.py
@@ -1,8 +1,12 @@
 import unittest
-
+from matFun import math
 
 class testMatFunction(unittest.TestCase):
+    def setUp(self):
+        self.matFun=math(5,3)
+
     def test_Area_Square(self):
-        area = self.math.Area_Square(3,5)
-        self.assertIn(20,area)
+        area =self.matFun.Area_Square()
+        self.assertEquals(20,area)
+
 
--- matFun.py
+++ matFun.py
@@ -1,4 +1,11 @@
 
 
-class matFunction():
+class math():
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def Area_Square(self):
+        a=self.l*self.w
+        return a
 
